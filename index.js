47391c22b19c6d2f5068bcef903cd3863497c883
massimogiorgiosopra <massimogiorgiosopra@gmail.com>
2025-07-23 10:37:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function registerContact(row) {
                   DeviceID: "06d51467-84b7-4835-982e-0570bbe71bbc",
                   DeviceToken: "12bkmVOlS7qEaHDCEbZlmM:APA91bHHkvpirhYr1oS1tXL5ZoVafy8EV3fmnQfHp3sbqNb_zyWhye57pBcO2RrbrkGAwu_a5MdXUUBs-GJpOIs8-SqQcABu1L6DWyK8EUGXiW2EBwG_xKU",
                   AppID: "d2bc490b-deeb-49df-b885-57c15c18f129",
-                  Platform: "Android",
+                  Platform: "Android OS",
                   ContactKey: "0030D00000nS6tWQAS"
                 }
               }
