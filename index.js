7fedd16abc53c3107e39ef0c77b0322aa2067f88
massimogiorgiosopra <massimogiorgiosopra@gmail.com>
2025-08-06 11:53:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const {
+  MC_CLIENT_ID,
+  MC_CLIENT_SECRET,
+  MC_SUBDOMAIN,
+  MC_DE_KEY,
+  MC_ACCOUNT_ID
+}
+
 const http = require('https');
 const init = {
   host: 'mc8d6gk0bxk851g6-g02k91bwbwy.auth.marketingcloudapis.com',
@@ -14,17 +22,16 @@ const callback = function(response) {
   });
   
   response.on('end', function() {
-  accessToken = result.data.access_token;
-  console.log(`🔷 access token: ${accessToken}`);  
+  console.log(result.toString());
   });
 };
 
 const req = http.request(init, callback);
 const body = `{
-  "grant_type": "client_credentials",
-  "client_id": "mjo118smj8gpxby9r43i46vw",
-  "client_secret": "PKHj46KTMh9sKxJN4iGyDdoC",
-  "account_id": "100006681"
+   grant_type: 'client_credentials',
+   client_id: MC_CLIENT_ID,
+   client_secret: MC_CLIENT_SECRET,
+   account_id: MC_ACCOUNT_ID
 }`;
 req.write(body);
 req.end();
