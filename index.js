77f00b75739cd3bfa09f06eb548c3425ae8f0e9d
massimogiorgiosopra <massimogiorgiosopra@gmail.com>
2025-08-06 14:47:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MC_CLIENT_SECRET = process.env.MC_CLIENT_SECRET;
 const MC_ACCOUNT_ID = process.env.MC_ACCOUNT_ID;
 const MC_SUBDOMAIN = process.env.MC_SUBDOMAIN;
   
-const auth = {
+const authcall = {
   host: `${MC_SUBDOMAIN}.auth.marketingcloudapis.com`,
   path: '/v2/token',
   method: 'POST',
@@ -13,45 +13,24 @@ const auth = {
     'content-type': 'application/json'
   }
 };
-const authresponse = function(response) {
-  let authresult = Buffer.alloc(0);
-  response.on('data', function(chunk) {
-    authresult = Buffer.concat([authresult, chunk]);
+const authresp = function(response) {
+  let authdata = Buffer.alloc(0);
+  authResponse.on('data', function(chunk) {
+    result = Buffer.concat([authdata, chunk]);
   });
   
-  response.on('end', function() {
-  console.log(authresult.toString());
+  authResponse.on('end', function() {
+  console.log(result.toString());
   });
 };
 
-const authRequest = http.request(auth, authresponse);
-const body = JSON.stringify({
+const authrequest = http.request(authcall, authresp);
+const authbody = JSON.stringify({
    grant_type: 'client_credentials',
    client_id: MC_CLIENT_ID,
    client_secret: MC_CLIENT_SECRET,
    account_id: MC_ACCOUNT_ID
 });
 
-const automation = {
-  host: `${MC_SUBDOMAIN}.rest.marketingcloudapis.com`,
-  path: '/automation/v1/automations/key:{key}/actions/runallonce',
-  method: 'POST',
-  headers: {
-    'Authorization': 'Bearer YOUR_ACCESS_TOKEN'
-  }
-};
-const callback = function(response) {
-  let result = Buffer.alloc(0);
-  response.on('data', function(chunk) {
-    result = Buffer.concat([result, chunk]);
-  });
-  
-  response.on('end', function() {
-    // result has response body buffer
-    console.log(result.toString());
-  });
-};
-const req = http.request(automation, callback);
 
-req.write(body);
 req.end();
